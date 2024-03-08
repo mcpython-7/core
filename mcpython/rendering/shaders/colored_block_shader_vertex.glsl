06661c28ada9cdf3eb091968ff7a0843e94a147b
uuk0 <baulukas1301@googlemail.com>
2024-03-08 13:46:15 +0100

    added shader for re-colored blocks
    this will be needed for grass blocks

---
+++ mcpython/rendering/shaders/colored_block_shader_vertex.glsl
@@ -0,0 +1,25 @@
+#version 330 core
+in vec3 position;
+in vec2 tex_coords;
+in vec4 colors;
+
+out vec2 texture_coords;
+out vec3 vertex_position;
+out vec4 coloring;
+
+uniform WindowBlock
+{
+    mat4 projection;
+    mat4 view;
+} window;
+
+uniform mat4 model;
+
+void main()
+{
+    vec4 pos = window.view * model * vec4(position, 1.0);
+    gl_Position = window.projection * pos;
+
+    vertex_position = pos.xyz;
+    texture_coords = tex_coords;
+}
--- mcpython/rendering/util.py
+++ mcpython/rendering/util.py
@@ -32,6 +32,7 @@ def create_shader_group(
 
 
 DEFAULT_BLOCK_SHADER, DEFAULT_BLOCK_GROUP = create_shader_group("default_block_shader")
+COLORED_BLOCK_SHADER, COLORED_BLOCK_GROUP = create_shader_group("colored_block_shader")
 COLORED_LINE_SHADER, COLORED_LINE_GROUP = create_shader_group("colored_outline_shader")
 
 
