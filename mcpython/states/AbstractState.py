ca17df92cb70fc43749fccd80c9a613e7828e23d
uuk0 <baulukas1301@googlemail.com>
2024-04-06 14:03:16 +0200

    state parts now have in general an 'enabled' property

--- mcpython/states/AbstractState.py
+++ mcpython/states/AbstractState.py
@@ -9,6 +9,24 @@ if typing.TYPE_CHECKING:
 
 
 class AbstractStatePart(ABC):
+    def __init__(self):
+        self._enabled = True
+
+    def get_enabled(self):
+        return self._enabled
+
+    def set_enabled(self, enabled: bool):
+        if self._enabled is enabled:
+            return
+
+        self._enabled = enabled
+        if not self._enabled:
+            self.on_deactivate()
+        else:
+            self.on_activate()
+
+    enabled = property(get_enabled, set_enabled)
+
     def on_activate(self):
         pass
 
@@ -56,7 +74,8 @@ class AbstractState(ABC):
         from mcpython.rendering.Window import Window
 
         for part in self.state_parts:
-            part.on_activate()
+            if part.enabled:
+                part.on_activate()
 
         # call an on_resize() event as the size might have changed in between
         self.on_resize(*Window.INSTANCE.get_size())
@@ -70,7 +89,8 @@ class AbstractState(ABC):
         from mcpython.rendering.Window import Window
 
         for part in self.state_parts:
-            part.on_deactivate()
+            if part.enabled:
+                part.on_deactivate()
 
         # Release all currently pressed keys and mouse buttons when leaving the state
         for key, state in Window.INSTANCE.key_state_handler.data.items():
@@ -83,42 +103,52 @@ class AbstractState(ABC):
 
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
         for part in self.state_parts:
-            part.on_mouse_press(x, y, button, modifiers)
+            if part.enabled:
+                part.on_mouse_press(x, y, button, modifiers)
 
     def on_mouse_release(self, x: int, y: int, button: int, modifiers: int):
         for part in self.state_parts:
-            part.on_mouse_release(x, y, button, modifiers)
+            if part.enabled:
+                part.on_mouse_release(x, y, button, modifiers)
 
     def on_mouse_motion(
         self, x: int, y: int, dx: int, dy: int, buttons: int, modifiers: int
     ):
         for part in self.state_parts:
-            part.on_mouse_motion(x, y, dx, dy, buttons, modifiers)
+            if part.enabled:
+                part.on_mouse_motion(x, y, dx, dy, buttons, modifiers)
 
     def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
         for part in self.state_parts:
-            part.on_mouse_scroll(x, y, scroll_x, scroll_y)
+            if part.enabled:
+                part.on_mouse_scroll(x, y, scroll_x, scroll_y)
 
     def on_key_press(self, symbol: int, modifiers: int):
         for part in self.state_parts:
-            part.on_key_press(symbol, modifiers)
+            if part.enabled:
+                part.on_key_press(symbol, modifiers)
 
     def on_key_release(self, symbol: int, modifiers: int):
         for part in self.state_parts:
-            part.on_key_release(symbol, modifiers)
+            if part.enabled:
+                part.on_key_release(symbol, modifiers)
 
     def on_text(self, text: str):
         for part in self.state_parts:
-            part.on_text(text)
+            if part.enabled:
+                part.on_text(text)
 
     def on_draw(self, window: Window):
         for part in self.state_parts:
-            part.on_draw(window)
+            if part.enabled:
+                part.on_draw(window)
 
     def on_resize(self, w: int, h: int):
         for part in self.state_parts:
-            part.on_resize(w, h)
+            if part.enabled:
+                part.on_resize(w, h)
 
     def on_tick(self, dt: float):
         for part in self.state_parts:
-            part.on_tick(dt)
+            if part.enabled:
+                part.on_tick(dt)
